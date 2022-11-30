abbc5a26cc757de0a0e7d85597c0d6737d4aab95
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-30 14:32:51 +0530

    Added: UC3 Parameter in path variable

--- src/main/java/com/hello/msg/controller/HelloWorldController.java
+++ src/main/java/com/hello/msg/controller/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.hello.msg.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -17,5 +14,10 @@ public class HelloWorldController {
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String hello(@RequestParam String name) {
         return "Hello " + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello " + name + "!!!";
     }
 }
