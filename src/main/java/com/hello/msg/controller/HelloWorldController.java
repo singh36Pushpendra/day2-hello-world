3164f0155089056eeecd8a7bf4047cbdefb31168
singh36Pushpendra <psthakur.1999@gmail.com>
2022-11-30 14:18:35 +0530

    Added: UC2 Get method and query parameter

--- src/main/java/com/hello/msg/controller/HelloWorldController.java
+++ src/main/java/com/hello/msg/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.hello.msg.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,10 @@ public class HelloWorldController {
     @RequestMapping(value = {"/", "/world", "/msg"})
     public String hello() {
         return "Hello from Bridgelabs!";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String hello(@RequestParam String name) {
+        return "Hello " + name + "!";
     }
 }
